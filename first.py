1833891d9e317dff7ee812cde88006baaef2f955
Jack Shorenstein <jackshorenstein@gmail.com>
2021-07-19 09:27:30 -0700

    day1

---
+++ first.py
@@ -0,0 +1,23 @@
+# Variable Example Code
+number = 1
+print(number)
+# Hello World!
+print("Hello World!")
+print(number + 5)
+# Input Code
+name = input("What is your name?")
+print("Hello " + name + "!")
+# Integers and Arithemetic
+print(5 + 3)
+print(5 - 3)
+print(5 * 3)
+print(5 / 3)
+print(5 // 3)
+print(5 % 3)
+number1 = 10
+number2 = 3
+fullnumber = number1 + number2
+print(number1 + number2)
+print(fullnumber)
+number1 /= 4
+print(number1)
